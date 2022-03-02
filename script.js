a608b619eabdac786eab0585683713dea5fe5773
Albedo <raincadence27@gmail.com>
2022-03-02 13:23:12 -0800

    today done, 5 day WIP

--- script.js
+++ script.js
@@ -3,15 +3,18 @@
 let lat = 35
 let lon = 139
 
-let locationInput = document.getElementById('cityName').value
+const searchButton = document.getElementById("confirmSearch")
 
-let searchButton = document.getElementById('confirmSearch')
+searchButton.addEventListener("click", locationAPI);
 
-searchButton.addEventListener('click', console.log('click'))
+function locationAPI() {
+  let locationInput = document.getElementById('cityName').value
+  document.getElementById('today').innerHTML = moment().format("MMM Do YYYY")
 
-let geoCodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=${' + locationInput + '}&limit=5&appid=f656a41670de8a8c5067987b0bdf5407'
+  console.log(locationInput)
 
-function locationApi() {
+  let geoCodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=${' + locationInput + '}&limit=5&appid=f656a41670de8a8c5067987b0bdf5407'
+
   fetch(geoCodingAPI)
 
     .then(function (response) {
@@ -19,41 +22,84 @@ function locationApi() {
     })
     .then(function (data) {
       console.log(data)
+      let lon = data[0].lon
+      let lat = data[0].lat
+      document.getElementById('locationName').innerHTML = data[0].name;
+
+      let weatherTodayAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=f656a41670de8a8c5067987b0bdf5407'
+
+      fetch(weatherTodayAPI)
+
+        .then(function (response) {
+          return response.json()
+        })
+
+        .then(function (data) {
+          console.log(data)
+
+          //temperature data
+          let kelvin
+          let celsius = () => kelvin - 273.15
+
+          kelvin = parseFloat(data.current.temp)
+          document.getElementById('temperatureToday').innerHTML = 'Temperature: ' + Math.floor(celsius()) + '\xB0C';
+
+          //humidity data
+          document.getElementById('humidityToday').innerHTML = 'Humidity: ' + data.current.humidity + '%'
+
+          //wind data
+          let rawSpeed
+          let windSpeed = () => rawSpeed * 2.2369
+
+          rawSpeed = data.current.wind_speed
+          document.getElementById('windToday').innerHTML = 'Wind Speed: ' + Math.floor(windSpeed()) + 'MPH'
+
+          //uvi data
+          let uviValue = data.current.uvi
+          document.getElementById('UVI').innerHTML = 'UV Index: ' + uviValue
+
+          if (uviValue > 10) {
+            document.getElementById('UVI').style.backgroundColor = 'purple'
+          }
+          if (uviValue < 11) {
+            document.getElementById('UVI').style.backgroundColor = 'red'
+          }
+          if (uviValue < 8) {
+            document.getElementById('UVI').style.backgroundColor = 'orange'
+          }
+          if (uviValue < 6) {
+            document.getElementById('UVI').style.backgroundColor = 'yellow'
+          }
+          if (uviValue < 3) {
+            document.getElementById('UVI').style.backgroundColor = 'green'
+          }
+
+        })
+
+      let fiveDayAPI = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=f656a41670de8a8c5067987b0bdf5407'
+
+      fetch(fiveDayAPI)
+
+        .then(function (response) {
+          return response.json()
+        })
+        .then(function(data) {
+          console.log(data)
+          document.getElementById('day1Date').innerHTML = moment().add(1, 'days').format('L')    
+          document.getElementById('day2Date').innerHTML = moment().add(2, 'days').format('L')
+          document.getElementById('day3Date').innerHTML = moment().add(3, 'days').format('L')
+          document.getElementById('day4Date').innerHTML = moment().add(4, 'days').format('L')
+          document.getElementById('day5Date').innerHTML = moment().add(5, 'days').format('L')
+
+          document.getElementById('weatherFuture1').innerHTML = data.list[1].weather[0].main
+          document.getElementById('weatherFuture2').innerHTML = data.list[2].weather[0].main
+          document.getElementById('weatherFuture3').innerHTML = data.list[3].weather[0].main
+          document.getElementById('weatherFuture4').innerHTML = data.list[4].weather[0].main
+          document.getElementById('weatherFuture5').innerHTML = data.list[5].weather[0].main
+        })
     })
 }
-
-console.log(document.getElementById('locationName').innerHTML)
 
 //section - weather api fetch
 
 // let weatherToday = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f656a41670de8a8c5067987b0bdf5407'
-
-let weatherTodayAPI = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=f656a41670de8a8c5067987b0bdf5407'
-
-fetch(weatherTodayAPI)
-
-  .then(function (response) {
-    return response.json()
-  })
-
-  .then(function (data) {
-    console.log(data)
-    document.getElementById('locationName').innerHTML = data.name
-
-    //temperature data
-    let kelvin
-    let celsius = () => kelvin - 273.15
-
-    kelvin = parseFloat(data.main.temp)
-    document.getElementById('temperatureToday').innerHTML = 'Temperature: ' + Math.floor(celsius()) + '\xB0C';
-
-    //humidity data
-    document.getElementById('humidityToday').innerHTML = 'Humidity: ' + data.main.humidity + '%'
-
-    //wind data
-    let rawSpeed
-    let windSpeed = () => rawSpeed * 2.2369
-
-    rawSpeed = data.wind.speed
-    document.getElementById('windToday').innerHTML = 'Wind Speed: ' + Math.floor(windSpeed()) + 'MPH'
-  })

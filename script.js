c2f95d60167d8374c158c69f210f19717085567a
Albedo <raincadence27@gmail.com>
2022-03-01 18:17:38 -0800

    working weather api

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-//weather api fetch
+//section - gps coordinate
 
 let lat = 35
 let lon = 139
 
-let weatherToday = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=f656a41670de8a8c5067987b0bdf5407';
+// let geoCodingAPI = 'https://api.myptv.com/geocoding/v1/locations/by-text?searchText=Oceanside%20&apiKey=YzRhMGVjOTgwNTJmNGRiOTg1ODViMzM2YzZjOGFmZjA6MTA2ZjY0MzQtYWFkZi00MjE4LTg4NmQtZWVhYmU3NzJiNTUy'
+
+// 'https://api.myptv.com/geocoding/v1/locations/by-text?searchText=aan%27t%20verlaat%2033f%20&apiKey=YOUR_API_KEY'
+
+// fetch(geoCodingAPI)
+//   .then(function (response) {
+//     return response.json()
+//   })
+//   .then(function(data) {
+//     console.log(data)
+//   })
+
+console.log(document.getElementById('locationName').innerHTML)
+
+//section - weather api fetch
 
 // let weatherToday = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f656a41670de8a8c5067987b0bdf5407'
 
-fetch(weatherToday)
-  .then(function(response) {
-    return response.json();
+let weatherTodayAPI = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=f656a41670de8a8c5067987b0bdf5407'
+
+fetch(weatherTodayAPI)
+  .then(function (response) {
+    return response.json()
   })
-  .then(function(data) {
+  .then(function (data) {
     console.log(data)
+    document.getElementById('locationName').innerHTML = data.name;
   })

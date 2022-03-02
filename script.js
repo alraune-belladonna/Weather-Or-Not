5068afd8c25dc5101ec81e2fe104406ed2bc2c07
Albedo <raincadence27@gmail.com>
2022-03-02 14:22:52 -0800

    done

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 //section - gps coordinate
-
-let lat = 35
-let lon = 139
-
 const searchButton = document.getElementById("confirmSearch")
 
 searchButton.addEventListener("click", locationAPI);
 
+locationAPI()
+
 function locationAPI() {
+
   let locationInput = document.getElementById('cityName').value
+  console.log(locationInput)
+  lastSearch = localStorage.getItem('lastSearch')
+
+  //search history
+  if (lastSearch === null || locationInput.length !== 0) {
+    let searchHistory = document.createElement('li')
+    document.getElementById('searchHistory').appendChild(searchHistory)
+    searchHistory.innerText = locationInput
+    console.log(locationInput)
+    localStorage.setItem('lastSearch', locationInput)
+  }
+  else {
+    locationInput = lastSearch
+  }
+
   document.getElementById('today').innerHTML = moment().format("MMM Do YYYY")
 
   console.log(locationInput)
@@ -96,8 +110,27 @@ function locationAPI() {
           document.getElementById('weatherFuture3').innerHTML = data.list[3].weather[0].main
           document.getElementById('weatherFuture4').innerHTML = data.list[4].weather[0].main
           document.getElementById('weatherFuture5').innerHTML = data.list[5].weather[0].main
+
+          let celsius1 = () => data.list[1].main.temp - 273.15
+          let celsius2 = () => data.list[2].main.temp - 273.15
+          let celsius3 = () => data.list[3].main.temp - 273.15
+          let celsius4 = () => data.list[4].main.temp - 273.15
+          let celsius5 = () => data.list[5].main.temp - 273.15
+
+          document.getElementById('tempFuture1').innerHTML = 'Temperature: ' + Math.floor(celsius1()) + '\xB0C';
+          document.getElementById('tempFuture2').innerHTML = 'Temperature: ' + Math.floor(celsius2()) + '\xB0C';
+          document.getElementById('tempFuture3').innerHTML = 'Temperature: ' + Math.floor(celsius3()) + '\xB0C';
+          document.getElementById('tempFuture4').innerHTML = 'Temperature: ' + Math.floor(celsius4()) + '\xB0C';
+          document.getElementById('tempFuture5').innerHTML = 'Temperature: ' + Math.floor(celsius5()) + '\xB0C';
+
+          document.getElementById('humidFuture1').innerHTML = 'Humidity: ' + data.list[1].main.humidity + '%'
+          document.getElementById('humidFuture2').innerHTML = 'Humidity: ' + data.list[2].main.humidity + '%'
+          document.getElementById('humidFuture3').innerHTML = 'Humidity: ' + data.list[3].main.humidity + '%'
+          document.getElementById('humidFuture4').innerHTML = 'Humidity: ' + data.list[4].main.humidity + '%'
+          document.getElementById('humidFuture5').innerHTML = 'Humidity: ' + data.list[5].main.humidity + '%'
         })
     })
+  locationInput = ''
 }
 
 //section - weather api fetch
